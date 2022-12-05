05235110519bc975907d04c4276542e8b671690f
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-12-05 15:45:20 -0800

    added auth files that were not already existing in the template

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,15 @@
 -- Use this file to define your SQL tables
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS github_users;
 -- The SQL in this file will be executed when you run `npm run setup-db`
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR,
+  password_hash VARCHAR NOT NULL,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL
+);
+
 CREATE TABLE github_users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     login TEXT NOT NULL,
