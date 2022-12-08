2c4e6e7387994854b7e4456ea32bd6830043658d
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-12-08 13:13:17 -0800

    added posts test, #GET should allow authenticated users to view posts

---
+++ __tests__/posts.test.js
@@ -0,0 +1,26 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+jest.mock('../lib/services/github');
+const agent = request.agent(app);
+
+describe('post routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('#GET allows authenticated user to view all posts', async () => {
+    await agent.get('/api/v1/github/callback?code=42');
+    const res = await agent.get('/api/v1/posts');
+
+    expect(res.status).toEqual(200);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      content: expect.any(String),
+      user_id: expect.any(String),
+    });
+  });
+  
+})
