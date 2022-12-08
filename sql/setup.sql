43f75434ff5bb7538cac3d3eeb18e32e5d9b9403
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-12-08 15:19:49 -0800

    all revelant tests (github and posts) passing

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,14 +1,15 @@
 -- Use this file to define your SQL tables
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS github_users CASCADE;
 DROP TABLE IF EXISTS posts CASCADE;
 -- The SQL in this file will be executed when you run `npm run setup-db`
--- CREATE TABLE users (
---   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---   email VARCHAR,
---   password_hash VARCHAR NOT NULL,
---   first_name VARCHAR NOT NULL,
---   last_name VARCHAR NOT NULL
--- );
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR,
+  password_hash VARCHAR NOT NULL,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL
+);
 
 CREATE TABLE github_users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
